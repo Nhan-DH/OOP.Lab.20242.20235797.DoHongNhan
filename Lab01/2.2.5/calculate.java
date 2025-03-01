a82dd01fa075cbed07cf61c3508a579fcd48d249
Nhan-DH <hongnhan474747@gmail.com>
2025-03-01 16:44:41 +0700

    Update calculate.java

--- Lab01/2.2.5/calculate.java
+++ Lab01/2.2.5/calculate.java
@@ -1,6 +1,6 @@
-package lab1;	
-import javax.swing.JOptionPane;
+package lab1;
 
+import javax.swing.JOptionPane;
 
 public class calculate {
 	public static void main(String[] args) {
@@ -12,12 +12,13 @@ public class calculate {
 
 		double num1 = Double.parseDouble(strNum1);
 		double num2 = Double.parseDouble(strNum2);
-        double difference = num1 - num2 ;
+		double difference = num1 - num2;
 		double sum = num1 + num2;
 		double product = num1 * num2;
 		String divisionResult = (num2 != 0) ? String.valueOf(num1 / num2) : "Không thể chia cho 0";
 
-		String message = "Tổng: " + sum + "\n"+"Hieu: " + difference + "\n"+ "Tích: " + product + "\n" + "Thương: " + divisionResult;
+		String message = "Tổng: " + sum + "\n" + "Hieu: " + difference + "\n" + "Tích: " + product + "\n" + "Thương: "
+				+ divisionResult;
 
 		JOptionPane.showMessageDialog(null, message, "Kết quả", JOptionPane.INFORMATION_MESSAGE);
 	}
