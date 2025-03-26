47ec52652c95837bc8d59e3b1420f9aed5a24269
Nhan-DH <hongnhan474747@gmail.com>
2025-03-26 22:22:41 +0700

    lab02

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -21,32 +21,41 @@ public class Cart {
 		if (itemsOrdered[0] == null) {
 			System.out.println("Cart hiện tại đã trống !!!");
 		} else {
-			int a = 0;
+			int a = -1;
 			for (int i = 0; i < qtyOrdered; i++) {
 				if (itemsOrdered[i] == y) {
-					System.out.println("Đã xóa DVD : " + y.getTitle() + " thành công !!!" + " ( -" + y.getCost() + " ) ");
+					System.out
+							.println("Đã xóa DVD : " + y.getTitle() + " thành công !!!" + " ( -" + y.getCost() + " ) ");
 					a = i;
 					break;
 				}
 			}
-		if(a == 0) {
-			System.out.println("DVD không tồn tại trong Cart");
-		}
-			
+			if (a == -1) {
+				System.out.println("DVD không tồn tại trong Cart");
+			}
 
-			for (int i1 = a; i1 < qtyOrdered; i1++) {
+			for (int i1 = a; i1 < qtyOrdered - 1; i1++) {
 				itemsOrdered[i1] = itemsOrdered[i1 + 1];
-				qtyOrdered--;
 
 			}
 		}
+		qtyOrdered--;
+		System.out.println("Giỏ hàng hiện tại là :");
+		for (int i = 0; i < qtyOrdered; i++) {
+			System.out.println((i + 1) + " . " + itemsOrdered[i].getTitle() + " giá : " + itemsOrdered[i].getCost());
+		}
+
 	}
 
-	public float totalCost() {
+	public void totalCost() {
+		System.out.println("Hóa đơn của bạn : ");
+		for (int i = 0; i < qtyOrdered; i++) {
+			System.out.println((i + 1) + " . " + itemsOrdered[i].getTitle() + " giá : " + itemsOrdered[i].getCost());
+		}
 		float sum = 0.00f;
 		for (int i = 0; i < qtyOrdered; i++) {
 			sum += itemsOrdered[i].getCost();
 		}
-		return sum;
+		System.out.printf("Tổng tiền cần thanh toán: %.2f\n", sum);
 	}
 }
